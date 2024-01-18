9a5eda003470d7df56be8799bb9561d2977d2e45
yamiranea <yamilarenea@gmail.com>
2024-01-18 12:26:57 +0100

    setting random letters from a to z with range

--- index.php
+++ index.php
@@ -15,9 +15,20 @@ if (!isset($_SESSION['word'])) {
         $_SESSION['gamesLost'] = 0;
     }
 }
+?>
 
+<form method="post" action="">
+    <select name="guess">
+        <?php
+        foreach (range('A', 'Z') as $letter) {
+            echo '<option value = "' . strtoupper($letter) . '">' . strtoupper($letter) . '</option>';
+        }
+        ?>
+    </select>
+    <input type="submit" name="submit" value="GUESS">
+</form>
 
-
+<?php
 for ($i = 0; $i < $wordLength; $i++) {
     if (in_array($word[$i], $guesses)) {
         echo $word[$i];
